5b085636ceca43ffd4dddac4c06cc48a7cc04d66
Abhishek Suresh <122855738+abhisheksuresh2004@users.noreply.github.com>
2025-02-11 11:20:49 -0800

    middleware still funky

---
+++ packages/api/src/middleware/errorHandler.ts
@@ -0,0 +1,26 @@
+export default class AppError extends Error {
+    statusCode: number;
+    status: string;
+    isOperational: boolean;
+  
+    constructor(message: string, statusCode: number) {
+      super(message);
+      this.statusCode = statusCode;
+      this.status = `${statusCode}`.startsWith('4') ? 'fail' : 'error';
+      this.isOperational = true;
+  
+      Error.captureStackTrace(this, this.constructor);
+    }
+  }
+  
+  export const errorHandler = (err: any, req: any, res: any, next: any) => {
+    err.statusCode = err.statusCode || 500;
+    err.status = err.status || 'error';
+  
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+      ...(process.env.NODE_ENV === 'development' && { stack: err.stack })
+    });
+  };
+  
